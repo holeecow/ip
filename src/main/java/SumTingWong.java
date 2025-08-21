5d3b19bc9c58903fd01cb67883e96a99ce3f76ae
zexuan <jasonteo032@gmail.com>
2025-08-22 02:19:27 +0800

    Add Task.java
    Modified SumTingWong.java to make use of Task objects

--- src/main/java/SumTingWong.java
+++ src/main/java/SumTingWong.java
@@ -1,9 +1,8 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class SumTingWong {
     public static void main(String[] args) {
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         String botName = "SumTingWong";
         int currentIndex = 0;
 
@@ -14,14 +13,16 @@ public class SumTingWong {
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
 
         String userInput = myObj.nextLine();
+
         while (!userInput.equals("bye")) {
-            tasks[currentIndex] = userInput;
+            Task task = new Task(userInput);
+            tasks[currentIndex] = task;
             currentIndex++;
 
             if (userInput.equals("list")) {
                 System.out.println("-------------------------------");
                 for (int i = 1; tasks[i] != null; i++) {
-                    System.out.println(i + ". " + tasks[i - 1]);
+                    System.out.println(i + ". " + tasks[i].getDiscription());
                 }
                 System.out.println("-------------------------------");
             } else {
