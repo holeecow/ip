c2cc0719bf9a89c4c2bf76001f7b8f85eacd8662
zexuan <jasonteo032@gmail.com>
2025-08-22 01:52:06 +0800

    Add ability to store text in SumTingWong.java
    Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/SumTingWong.java
+++ src/main/java/SumTingWong.java
@@ -1,8 +1,12 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SumTingWong {
     public static void main(String[] args) {
+        String[] tasks = new String[100];
         String botName = "SumTingWong";
+        int currentIndex = 0;
+
         System.out.println("------------------------------- \n");
         System.out.println("Hello! I'm " + botName + "\n"
                             + "What can I do for you? -.-");
@@ -11,11 +15,24 @@ public class SumTingWong {
 
         String userInput = myObj.nextLine();
         while (!userInput.equals("bye")) {
-            System.out.println("------------------------------- \n");
-            System.out.println(userInput + ": D");
-            System.out.println("------------------------------- \n");
+            tasks[currentIndex] = userInput;
+            currentIndex++;
+
+            if (userInput.equals("list")) {
+                System.out.println("-------------------------------");
+                for (int i = 1; tasks[i] != null; i++) {
+                    System.out.println(i + ". " + tasks[i - 1]);
+                }
+                System.out.println("-------------------------------");
+            } else {
+                System.out.println("------------------------------- \n");
+                System.out.println("added: " + userInput);
+                System.out.println("------------------------------- \n");
+            }
+
             userInput = myObj.nextLine();
         }
+
         System.out.println("------------------------------- \n");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("------------------------------- \n");
