985af2cea9ab9fa4eaf4925f5e5f1ea3015ffd27
zexuan <jasonteo032@gmail.com>
2025-08-21 17:27:58 +0800

    Improve on the skeletal version of Sumtingwong
    Sumtingwong now echos commands entered by the user, and exits when the user types the command "bye"

--- src/main/java/SumTingWong.java
+++ src/main/java/SumTingWong.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class SumTingWong {
     public static void main(String[] args) {
         String botName = "SumTingWong";
+        System.out.println("------------------------------- \n");
         System.out.println("Hello! I'm " + botName + "\n"
-                            + "What can I do for you? -.-\n"
-                            + "------------------------------- \n"
-                            + "Bye. Hope to see you again soon! \n"
-                            + "-------------------------------");
+                            + "What can I do for you? -.-");
+        System.out.println("------------------------------- \n");
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+
+        String userInput = myObj.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println("------------------------------- \n");
+            System.out.println(userInput + ": D");
+            System.out.println("------------------------------- \n");
+            userInput = myObj.nextLine();
+        }
+        System.out.println("------------------------------- \n");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("------------------------------- \n");
     }
 }
