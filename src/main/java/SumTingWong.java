98029f731f141e716b4648dc46c05d3a664c25e5
zexuan <jasonteo032@gmail.com>
2025-08-21 17:08:07 +0800

    Add emoji to greeting in SumTingWong.java

--- src/main/java/SumTingWong.java
+++ src/main/java/SumTingWong.java
@@ -1,8 +1,8 @@
 public class SumTingWong {
     public static void main(String[] args) {
-        String botName = "Sum Ting Wong";
+        String botName = "SumTingWong";
         System.out.println("Hello! I'm " + botName + "\n"
-                            + "What can I do for you?\n"
+                            + "What can I do for you? -.-\n"
                             + "------------------------------- \n"
                             + "Bye. Hope to see you again soon! \n"
                             + "-------------------------------");
