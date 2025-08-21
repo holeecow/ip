901f40c64959eb98d6a08d43a82cb8f74f38ec4d
zexuan <jasonteo032@gmail.com>
2025-08-21 16:47:35 +0800

    Add SumTingWong.java

---
+++ src/main/java/SumTingWong.java
@@ -0,0 +1,10 @@
+public class SumTingWong {
+    public static void main(String[] args) {
+        String botName = "Sum Ting Wong";
+        System.out.println("Hello! I'm " + botName + "\n"
+                            + "What can I do for you?\n"
+                            + "------------------------------- \n"
+                            + "Bye. Hope to see you again soon! \n"
+                            + "-------------------------------");
+    }
+}
