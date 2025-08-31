9807d2c01488e7b219dc78996b873ba30a2b15e3
zexuan <jasonteo032@gmail.com>
2025-09-01 01:33:18 +0800

    Add support for java LocalDate and LocalTime
    Update Deadline.java to store the deadline as java.time.LocalDate and
    java.time.LocalTime objects.
    
    Update SumTingWong.java to accept dates in yyyy-mm-dd format
    and print in MMM dd yyyy format

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,10 +1,25 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
     protected String by;
 
+    protected LocalDate date;
+
+    protected LocalTime time;
+
     public Deadline(String description, String by, boolean isDone) {
         super(description, isDone);
         this.by = by;
+    }
+
+    public Deadline(String description, LocalTime time, LocalDate date, boolean isDone) {
+        super(description, isDone);
+        this.time = time;
+        this.date = date;
+        this.by = "";
     }
 
     public String toFileFormat() {
@@ -13,6 +28,9 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return this.by.isEmpty()
+                ? "[D]" + super.toString() + " (by: " + this.date.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + " "
+                    + this.time.format(DateTimeFormatter.ofPattern("h:mm a")) + ")"
+                : "[D]" + super.toString() + " (by: " + by + ")";
     }
 }
