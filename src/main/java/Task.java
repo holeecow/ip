d4e5f564694d871540e23f41f1e32c8644279225
holeecow <jasonteo032@gmail.com>
2025-08-31 21:46:39 +0800

    Add loadTasks() function to TaskManager.java
    Add a function fromFileFormat() in Task.java to support the loadTasks()
    function. fromFileFormat takes in a line from the TaskList.txt file,
    reads the line and returns the correct Event, Deadline or Todo object
    depending on what the task is.
    
    Update constructor of Task.java to take in a boolean isDone param
    to support the fromFileFormat() function.
    
    Update ToDo.java, Deadline.java and Event.java to have their respective
    toFileFormat() function which converts the event into a file format to
    be stored in TaskList.txt

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,9 +2,9 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
-    public Task(String description) {
+    public Task(String description, boolean isDone) {
         this.description = description;
-        this.isDone = false;
+        this.isDone = isDone;
     }
 
     // mark done task with X
@@ -23,6 +23,26 @@ public class Task {
 
     public void markAsNotDone() {
         this.isDone = false;
+    }
+
+    public static Task fromFileFormat(String line) {
+        String[] parts = line.split(" \\| ");
+        String typeOfTask = parts[0].trim();
+        Boolean isDone = Boolean.valueOf(parts[1].trim());
+        String description = parts[2].trim();
+
+        if (typeOfTask.equals("T")) {
+            return new ToDo(description, isDone);
+        } else if (typeOfTask.equals("D")) {
+            String deadline = parts[3].trim();
+            return new Deadline(description, deadline, isDone);
+        }
+
+        // task is an Event
+        String from = parts[3].trim();
+        String to = parts[4].trim();
+        return new Event(description, from, to, isDone);
+
     }
 
     @Override
--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,4 +1,6 @@
 import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.File;
 import java.io.IOException;
@@ -24,7 +26,16 @@ public class TaskManager {
             // write tasks to the file
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
                 for (Task task : tasks) {
-                    writer.write(task.toString());
+                    if (task.getClass() == ToDo.class){
+                        ToDo todo = (ToDo) task;
+                        writer.write(todo.toFileFormat());
+                    } else if(task.getClass() == Deadline.class){
+                        Deadline deadline = (Deadline) task;
+                        writer.write(deadline.toFileFormat());
+                    } else {
+                        Event event = (Event) task;
+                        writer.write(event.toFileFormat());
+                    }
                     writer.newLine();
                 }
                 // make sure that all data is written
@@ -33,5 +44,29 @@ public class TaskManager {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+    }
+    public static ArrayList<Task> loadTasks() {
+        ArrayList<Task> tasks = new ArrayList<>();
+
+        try {
+            File file = new File(FILE_PATH);
+
+            // if the file doesn't exist, return empty task list
+            if (!file.exists()) {
+                return tasks;
+            }
+
+            // read tasks from the file and add it to tasks
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    Task task = Task.fromFileFormat(line);
+                    tasks.add(task);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return tasks;
     }
 }
